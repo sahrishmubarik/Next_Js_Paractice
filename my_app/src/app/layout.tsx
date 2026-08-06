55a9fa7974cb465712b63f029251ed37b0faa32b
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-08-06 12:41:40 +0500

    Add Header and Footer in Layout

--- my_app/src/app/layout.tsx
+++ my_app/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link  from 'next/link';
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -21,33 +22,13 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
 <html lang="en">
       <body className="min-h-screen flex flex-col">
-        <header className="bg-purple-500 text-white p-4 flex justify-center items-center">
-        <h1 className="text-xl font-bold ml-1 mr-2">My App</h1>
-        <nav className="ml-4 flex gap-1">
-          <Link href="/" className="mr-4 hover:underline">
-            Home
-          </Link>
-           <Link href='/about' className="mr-4  hover:underline">
-            About
-          </Link>
-          <Link href='/products' className=" mr-4 hover:underline">
-            Products
-          </Link>
-          <Link href="/blog" className="mr-4  hover:underline">
-            Blog
-          </Link>
-         
-        </nav>
-
-        </header>
-
+        <Header />
         <main className="flex-1 flex justify-center items-center">
           {children}
         </main>
 
-        <footer className="bg-purple-500 text-white p-4 flex justify-center items-center">
-          Footer
-        </footer>
+        <Footer/>
+    
       </body>
     </html>
   );
