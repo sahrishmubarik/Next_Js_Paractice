5e736852a1897d953288b8aa2533fbe6ce15c0cb
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-08-26 16:45:54 +0500

    update dashboard page

--- layout_template/app/dashboard/page.tsx
+++ layout_template/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 export default  async function Dashboard() {
-    await new Promise((resolve)=> setTimeout(resolve,3000));
+    await new Promise((resolve)=> setTimeout(resolve,1000));
+      // throw new  Error("Fail to load dashboard");
   return (
     <div className="bg-white rounded-2xl shadow-sm p-8">
       <h2 className="text-3xl font-bold text-gray-900">
