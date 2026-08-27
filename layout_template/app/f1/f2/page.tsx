f7aee8291acb29d1d6c89dd2bf8b0ad6823652fb
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-08-27 17:10:52 +0500

    feat: add f4 link to f2 page

--- layout_template/app/f1/f2/page.tsx
+++ layout_template/app/f1/f2/page.tsx
@@ -1,9 +1,13 @@
-
-
+import Link from "next/link"
 export default function F1(){
 
     return(
         <>
-     <h1 className="font-bold text-2xl">F2 page</h1></>
+     <h1 className="font-bold text-2xl">F2 page</h1>
+     <div>
+         <Link href="/f4" className="text-blue-600"> F4</Link>
+       </div>
+       </>
+
     )
 }
