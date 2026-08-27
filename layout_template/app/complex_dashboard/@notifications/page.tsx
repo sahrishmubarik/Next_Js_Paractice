cf778ea3fdba8ac8acdbaf07290405e4536c23bd
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-08-27 12:00:46 +0500

    feat: add archived notifications route

--- layout_template/app/complex_dashboard/@notifications/page.tsx
+++ layout_template/app/complex_dashboard/@notifications/page.tsx
@@ -1,7 +1,33 @@
-import { Card } from "@/app/components/card"
+"use client";
+import { Card } from "@/app/components/card";
+import { useRouter } from "next/navigation";
 
-export default function notifications(){
-    return(
-        <Card>Notifications</Card>
-    )
+export default function Notifications() {
+  const router = useRouter();
+
+  const handleArchived = () => {
+    router.push("/complex_dashboard/archived");
+    router.refresh();
+  };
+
+  return (
+    <Card>
+      <div className="flex flex-col">
+        <div className="font-extrabold text-3xl">
+          Notifications
+        </div>
+
+        <h1 className="font-semibold">
+          If you want to get archived then click on it
+        </h1>
+
+        <button
+          onClick={handleArchived}
+          className="font-semibold text-blue-600 underline"
+        >
+          Archived
+        </button>
+      </div>
+    </Card>
+  );
 }
