efd7eec0718f3a82e0939d6bb00f89718059e1c4
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-08-27 12:00:27 +0500

    feat: update complex dashboard layout

--- layout_template/app/complex_dashboard/layout.tsx
+++ layout_template/app/complex_dashboard/layout.tsx
@@ -1,31 +1,32 @@
-
 import React from "react";
 
 export default function complexDashboardLayout({
-    children,
-    userAnalytics,
-    revenue,
-    notifications
-}:{children:React.ReactNode;
-    userAnalytics:React.ReactNode;
-    revenue:React.ReactNode;
-    notifications:React.ReactNode;
-
-
-
-}){
-return(
+  children,
+  userAnalytics,
+  revenue,
+  notifications,
+  login,
+}: {
+  children: React.ReactNode;
+  userAnalytics: React.ReactNode;
+  revenue: React.ReactNode;
+  notifications: React.ReactNode;
+  login: React.ReactNode;
+}) {
+  const isLoggedIn = true;
+  return isLoggedIn ? (
     <>
-    <div>{children}</div>
-    <div className=" flex">
+      <div>{children}</div>
+      <div className=" flex">
         <div className=" flex flex-col">
-            <div>{userAnalytics}</div>
-            <div>{revenue}</div>
+          <div>{userAnalytics}</div>
+          <div>{revenue}</div>
         </div>
-      
-            <div className="flex flex-1">{notifications}</div>
-        
-    </div>
+
+        <div className="flex flex-1">{notifications}</div>
+      </div>
     </>
-)
+  ) : (
+    login
+  );
 }
