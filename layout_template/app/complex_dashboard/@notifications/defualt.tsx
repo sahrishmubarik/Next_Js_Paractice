28588b7d4a292465fc8f5f9b653eb9e40282f4ab
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-08-27 12:01:43 +0500

    feat: add default UI for parallel routes

---
+++ layout_template/app/complex_dashboard/@notifications/defualt.tsx
@@ -0,0 +1,8 @@
+import { Card } from "@/app/components/card";
+
+export default function notificationDefault(){
+    return(<>
+    <Card>
+        <h1>Default view </h1>
+        <h1> Notification.</h1></Card> </>)
+}
