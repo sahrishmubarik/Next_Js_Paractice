6b2eb5290f2e0bb7cab9098dfcbcde83fbdb9c8b
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-09-01 17:28:22 +0500

    feat: add navigation and image slider components

---
+++ rendering/src/components/navLink.tsx
@@ -0,0 +1,9 @@
+
+export function NavLink(){
+    return(
+    <>
+   <a href="/about" className="text-black hover:underline ml-4">About</a>
+    <a href="/product" className="text-black hover:underline ml-4">Products</a>
+    </>
+    )
+}
