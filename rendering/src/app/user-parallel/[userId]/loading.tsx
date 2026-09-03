7eef03b56debf18b6a5f2b2cf47efb772c2b4812
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-09-03 12:00:18 +0500

    Add parallel data fetching example

---
+++ rendering/src/app/user-parallel/[userId]/loading.tsx
@@ -0,0 +1,8 @@
+export default async function Loading(){
+    return(
+        <div className="flex justify-center items-center h-screen">
+            <div className="animate-spin rounded-full h-25 w-25 border-t-2 border-b-2 border-blue-500">
+            </div>
+        </div>
+    )
+}
